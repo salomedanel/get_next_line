8ae0236ba4d196640d9834606a2c6ad908c6bd9b
Salome DANEL <sdanel@e1r11p19.clusters.42paris.fr>
2023-02-10 10:47:25 +0100

    push

---
+++ get_next_line_bonus.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sdanel <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/23 14:32:47 by sdanel            #+#    #+#             */
+/*   Updated: 2022/11/29 18:08:57 by sdanel           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+# endif
+
+int		ft_strlen(char *str);
+char	*ft_strchr(char *s, int c);
+char	*ft_strjoin(char *s1, char *s2);
+int		findnl(char *stash);
+char	*ft_read(int fd, char *stash);
+char	*clean_stash(char *stash, int index);
+char	*ft_get_line(char *stash, char *line);
+char	*get_next_line(int fd);
+
+#endif
